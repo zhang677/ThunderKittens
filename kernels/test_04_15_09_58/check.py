bdbec541a2ca7564ddf409af8e456b6e05cfd06d
zhang677 <ZhangghLCN@163.com>
2025-04-20 17:15:46 -0700

    Add test_04_15_09_58

---
+++ kernels/test_04_15_09_58/check.py
@@ -0,0 +1,20 @@
+import test_04_15_09_58
+import torch
+import torch.nn.functional as F
+
+
+torch.manual_seed(42)
+B, H, M, N, D = 13, 8, 16, 512, 128
+q = torch.randn((B, M, H, D), device='cuda', dtype=torch.bfloat16)
+k = torch.randn((B, N, H, D), device='cuda', dtype=torch.bfloat16)
+v = torch.randn((B, N, H, D), device='cuda', dtype=torch.bfloat16)
+is_causal = False
+# Enable FlashAttention and disable other implementations
+with torch.backends.cuda.sdp_kernel(enable_flash=True, enable_math=False, enable_mem_efficient=False):
+    ref_output = F.scaled_dot_product_attention(
+        q, k, v,
+        is_causal=is_causal    # Optional
+    )
+
+output = torch.zeros_like(ref_output)
+test_04_15_09_58.wrapped_attend_ker(q, k, v, output)
---
+++ kernels/test_04_15_09_58/test_04_15_09_58.cu
@@ -0,0 +1,130 @@
+#include <iostream>
+#include "kittens.cuh"
+#include "pyutils/pyutils.cuh"
+
+#define CudaCheckError()    __cudaCheckError( __FILE__, __LINE__ )
+inline void __cudaCheckError( const char *file, const int line ) {
+    cudaError err = cudaGetLastError();
+    if ( cudaSuccess != err )
+    {
+        fprintf( stderr, "cudaCheckError() failed at %s:%i : %s\n",
+                 file, line, cudaGetErrorString( err ) );
+        exit( -1 );
+    }
+    // More careful checking. However, this will affect performance.
+    // Comment away if needed.
+    err = cudaDeviceSynchronize();
+    if( cudaSuccess != err )
+    {
+        fprintf( stderr, "cudaCheckError() with sync failed at %s:%i : %s\n",
+                 file, line, cudaGetErrorString( err ) );
+        exit( -1 );
+    }
+}
+
+using namespace kittens;
+
+constexpr int PIPE_STAGES = 2;
+constexpr int TILE_SIZE_N = 16;
+constexpr int TILE_SIZE_D = 128;
+constexpr int QO_SEQ = TILE_SIZE_N;
+constexpr int KV_BLOCKS = 32
+constexpr int KV_SEQ = TILE_SIZE_N * KV_BLOCKS;
+template<typename T=bf16, typename L=row_l> using qkvo_tile = rt<T, TILE_SIZE_N, TILE_SIZE_N, L>;
+template<typename T=float> using attn_tile = rt<T, TILE_SIZE_D, TILE_SIZE_D>;
+using shared_tile = st_bf<TILE_SIZE_D, TILE_SIZE_N>;
+using global_kv_layout = gl<bf16, -1, KV_SEQ, -1, TILE_SIZE_D>; // batch, depth, row, col
+using global_qo_layout = gl<bf16, -1, QO_SEQ, -1, TILE_SIZE_D>;
+struct globals {
+    global_kv_layout Kg, Vg;
+    global_qo_layout Qg, Og;
+    dim3 grid()  { return dim3(Qg.batch, Qg.rows); }
+};
+
+
+__global__ void __launch_bounds__(WARP_THREADS, 1)
+attend_ker(const __grid_constant__ globals g) {
+    const int batch = blockIdx.y, head = blockIdx.x;
+
+    extern __shared__ alignment_dummy __shm[];
+    shared_allocator al((int*)&__shm[0]);
+
+    shared_tile (&k_smem)[PIPE_STAGES] = al.allocate<shared_tile, PIPE_STAGES>();
+    shared_tile (&v_smem)[PIPE_STAGES] = al.allocate<shared_tile, PIPE_STAGES>();
+    shared_tile (&qo_smem)[1] = al.allocate<shared_tile, 1>();
+
+    qkvo_tile<bf16> q_reg, k_reg;
+    qkvo_tile<bf16, col_l> v_reg;
+    qkvo_tile<float> o_reg;
+    attn_tile<float> att_block;
+    attn_tile<bf16> att_block_mma;
+    typename attn_tile<float>::col_vec max_vec_last, max_vec, norm_vec;
+    // going through shared memory improves coalescing of dram reads.
+    load<1, false>(qo_smem[0], g.Qg, {batch, 0, head, 0});
+    __syncwarp();
+    load(q_reg, qo_smem[0]);
+    __syncthreads();
+    if constexpr(D == 128) q_reg *= __float2bfloat16(0.08838834764f * 1.44269504089f);
+
+    max_vec = base_types::constants<float>::neg_infty();
+    norm_vec = 0.f;
+    o_reg = 0.f;
+    // launch the load of the first k, v tiles
+    int tic = 0;
+    load_group::load_async<1, false>(k_smem[0][0], g.Ka, {batch, 0, head, 0});
+    load_group::load_async<1, false>(v_smem[0][0], g.Va, {batch, 0, head, 0});
+    for (auto kv_idx = 0; kv_idx < KV_BLOCKS; kv_idx++, tic=(tic + 1) % PIPE_STAGES) {
+        int next_load_idx = kv_idx + 1;
+        if (next_load_idx * TILE_SIZE_N < g.Kg.depth()) {
+            int next_tic = (tic + 1) % PIPE_STAGES;
+            load_group::load_async<1, false>(k_smem[0][next_tic], g.Kg, {batch, next_load_idx, head, 0});
+            load_group::load_async<1, false>(v_smem[0][next_tic], g.Vg, {batch, next_load_idx, head, 0});
+            load_async_wait<1>();
+        }
+        else load_async_wait();
+        __syncthreads();
+
+        load(k_reg, k_smem[0][tic]);
+        att_block = 0.f;
+        mma<transpose::N, transpose::T>(att_block, q_reg, k_reg, att_block); // Q@K.T
+        max_vec_last = max_vec;
+        max_vec = max<axis::COL>(att_block, max_vec); 
+        att_block = exp2(att_block - max_vec); 
+        max_vec_last = exp2(max_vec_last - max_vec);
+        norm_vec *= max_vec_last; 
+        norm_vec = sum<axis::COL>(att_block, norm_vec); 
+        att_block_mma = att_block; 
+        load(v_reg, v_smem[subtile][tic]); 
+        o_reg *= max_vec_last; 
+        mma<transpose::N, transpose::N>(o_reg, att_block_mma, v_reg, o_reg);
+    }
+
+    o_reg /= norm_vec;
+    __syncthreads();
+    store(qo_smem[0], o_reg);
+    __syncwarp();
+    store<1, false>(g.Og, qo_smem[0], {batch, 0, head, 0});
+}
+
+void run_attend_ker(globals g) {
+    unsigned long mem_size = (kittens::MAX_SHARED_MEMORY) / 2;
+    cudaFuncSetAttribute(
+        attend_ker,
+        cudaFuncAttributeMaxDynamicSharedMemorySize,
+        mem_size
+    );
+    cudaDeviceSynchronize();
+    attend_ker<<<g.grid(), WARP_THREADS, mem_size>>>(g);
+    cudaDeviceSynchronize();
+    CudaCheckError();
+    cudaError_t err = cudaGetLastError();
+    if (err != cudaSuccess) {
+        std::cout << "CUDA error: " << cudaGetErrorString(err) << std::endl;
+        return;
+    }
+}
+
+PYBIND11_MODULE(test_04_15_09_58, m) {
+    m.doc() = "test_04_15_09_58 python module";
+    py::bind_function<run_attend_ker>(m, "wrapped_attend_ker", &globals::Qg, &globals::Kg, &globals::Vg, &globals::Og);
+}

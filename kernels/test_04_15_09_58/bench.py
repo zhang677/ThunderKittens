aa86e0fab49a9dbf8a4a4e366e23c7abb1075f8c
zhang677 <zgh23@stanford.edu>
2025-04-22 18:13:49 +0000

    Refactor bench

--- kernels/test_04_15_09_58/bench.py
+++ kernels/test_04_15_09_58/bench.py
@@ -5,21 +5,27 @@ import torch
 
 torch.manual_seed(42)
 B, H, N = 13, 8, 512
-for M in [16, 32]:
-    for D in [64, 128]:
-        q = torch.randn((B, M, H, D), device='cuda', dtype=torch.bfloat16)
-        k = torch.randn((B, N, H, D), device='cuda', dtype=torch.bfloat16)
-        v = torch.randn((B, N, H, D), device='cuda', dtype=torch.bfloat16)
 
-        output = torch.zeros_like(q)
-        if M == 32 and D == 64:
-            test_04_15_09_58.wrapped_attend_ker_32_64(q, k, v, output)
-        elif M == 16 and D == 64:
-            test_04_15_09_58.wrapped_attend_ker_16_64(q, k, v, output)
-        elif M == 32 and D == 128:
-            test_04_15_09_58.wrapped_attend_ker_32_128(q, k, v, output)
-        elif M == 16 and D == 128:
-            test_04_15_09_58.wrapped_attend_ker_16_128(q, k, v, output)
-        else:
-            print("Unsupported configuration")
-            continue
+if len(sys.argv) != 3:
+    print("Usage: python bench.py <M> <D>")
+    sys.exit(1)
+else:
+    M = int(sys.argv[1])
+    D = int(sys.argv[2])
+
+q = torch.randn((B, M, H, D), device='cuda', dtype=torch.bfloat16)
+k = torch.randn((B, N, H, D), device='cuda', dtype=torch.bfloat16)
+v = torch.randn((B, N, H, D), device='cuda', dtype=torch.bfloat16)
+
+output = torch.zeros_like(q)
+if M == 32 and D == 64:
+    test_04_15_09_58.wrapped_attend_ker_32_64(q, k, v, output)
+elif M == 16 and D == 64:
+    test_04_15_09_58.wrapped_attend_ker_16_64(q, k, v, output)
+elif M == 32 and D == 128:
+    test_04_15_09_58.wrapped_attend_ker_32_128(q, k, v, output)
+elif M == 16 and D == 128:
+    test_04_15_09_58.wrapped_attend_ker_16_128(q, k, v, output)
+else:
+    print("Unsupported configuration")
+    continue

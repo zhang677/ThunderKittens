858f303d9bf987e645eba51d9e5ab8627348a64a
zhang677 <zgh23@stanford.edu>
2025-04-22 17:24:01 +0000

    Fix sweep script

--- kernels/test_04_15_09_58/extract_latency.py
+++ kernels/test_04_15_09_58/extract_latency.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 def extract_latency(problem_shape, profile_file, summary_file):
     df = pd.read_csv(profile_file)
-    latency_data = df[df['Metric Name'] == 'Elapsed Cycles' and df['Section Name'] == 'GPU Speed Of Light Throughput']
+    latency_data = df[(df['Metric Name'] == 'Elapsed Cycles') & (df['Section Name'] == 'GPU Speed Of Light Throughput')]
     with open(summary_file, 'a') as f:
         writer = csv.writer(f)
         writer.writerow([problem_shape, latency_data['Metric Value'].values[0]])

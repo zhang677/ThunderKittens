4ad1afe6ac349b0f976d6112eb372e84ea66e12c
zhang677 <zgh23@stanford.edu>
2025-04-22 01:24:54 +0000

    Add readme

--- kernels/test_04_15_09_58/README.md
+++ kernels/test_04_15_09_58/README.md
@@ -3,3 +3,5 @@ Modify the Makefile of 4090.cu on Runpod to align the python version with python
 pip install pybind11
 
 ncu --import ncu_report.ncu-rep --kernel-name "attend_ker" --csv --page details --log-file output_details.csv
+
+python filter_csv.py output_details.csv output_attend_ker.csv
--- kernels/test_04_15_09_58/bench.py
+++ kernels/test_04_15_09_58/bench.py
@@ -1,15 +1,11 @@
 import sys
 import os
-cur_path = "/workspace/ThunderKittens/kernels/test_04_15_09_58"
-print(cur_path)
-sys.path.append(cur_path) 
-
 import test_04_15_09_58
 import torch
 
 
 torch.manual_seed(42)
-B, H, M, N, D = 13, 8, 16, 512, 128
+B, H, M, N, D = 13, 8, 32, 512, 128
 q = torch.randn((B, M, H, D), device='cuda', dtype=torch.bfloat16)
 k = torch.randn((B, N, H, D), device='cuda', dtype=torch.bfloat16)
 v = torch.randn((B, N, H, D), device='cuda', dtype=torch.bfloat16)

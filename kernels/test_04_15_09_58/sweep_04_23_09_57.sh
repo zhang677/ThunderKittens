f8c88947b637b7dfac94b55a7bc11fd149a2123f
zhang677 <zgh23@stanford.edu>
2025-04-28 22:43:18 +0000

    Add static freq

--- kernels/test_04_15_09_58/sweep_04_23_09_57.sh
+++ kernels/test_04_15_09_58/sweep_04_23_09_57.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-
-mkdir -p profile_results
+BASE_DIR=profil_results_l40s
+mkdir -p $BASE_DIR
 
 B=17
 H=8
 
 for M in 16 32 48 64; do
     for D in 64 96 128 160; do
-        SUMMARY_FILE="profile_results/output_${B}x${H}x${M}x${D}.csv"
+        SUMMARY_FILE="$BASE_DIR/output_${B}x${H}x${M}x${D}.csv"
         if [ -f $SUMMARY_FILE ]; then
             echo "Summary file $SUMMARY_FILE already exists. Skipping..."
             continue
@@ -19,19 +19,19 @@ for M in 16 32 48 64; do
             N=$((tN * 16))
             PROBLEM_SHAPE="${B}x${H}x${M}x${N}x${D}"
             echo "Running $PROBLEM_SHAPE"
-            ncu --export profile_results/ncu_report -f --set full --target-processes all python run_once.py $B $H $M $N $D
+            ncu --launch-skip 4 --launch-count 1 --export $BASE_DIR/ncu_report -f --set full --target-processes all python run_once.py $B $H $M $N $D
 
             echo "Extracting metrics"
-            ncu --import profile_results/ncu_report.ncu-rep --kernel-name "attend_ker" --csv --page details --log-file profile_results/output_$PROBLEM_SHAPE.csv
+            ncu --import $BASE_DIR/ncu_report.ncu-rep --kernel-name "attend_ker" --csv --page details --log-file $BASE_DIR/output_$PROBLEM_SHAPE.csv
 
             echo "Processing metrics"
-            python filter_output.py profile_results/output_$PROBLEM_SHAPE.csv profile_results/short_$PROBLEM_SHAPE.csv
+            python filter_output.py $BASE_DIR/output_$PROBLEM_SHAPE.csv $BASE_DIR/short_$PROBLEM_SHAPE.csv
 
             # Extract the latency from the short CSV file
-            python extract_latency.py $PROBLEM_SHAPE profile_results/short_$PROBLEM_SHAPE.csv $SUMMARY_FILE
+            python extract_latency.py $PROBLEM_SHAPE $BASE_DIR/short_$PROBLEM_SHAPE.csv $SUMMARY_FILE
 
             # Clean up the intermediate files
-            rm profile_results/output_$PROBLEM_SHAPE.csv
+            rm $BASE_DIR/output_$PROBLEM_SHAPE.csv
         done
     done
 done
